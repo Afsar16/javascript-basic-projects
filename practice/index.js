fc8bcc1240031233e5beb276b107f2ecb81d5ac3
Afsar16 <118073955+Afsar16@users.noreply.github.com>
2023-04-27 22:21:12 +0530

    Add files via upload

---
+++ practice/index.js
@@ -0,0 +1,27 @@
+// const heading = document.getElementById("heading");
+// const mybtn = document.getElementById("mybtn");
+// const box1 = document.getElementById("box1");
+
+// //event listner example programme 
+
+// mybtn.addEventListener("click", () => {
+//     // console.log("clicked");
+                    
+    
+// });
+
+
+
+
+
+// #11 th vedio storage and session storage
+
+
+const input = document.getElementById("myinput");
+const btn = document.getElementById("mybtn");
+
+const btnclick = (e) => {
+    alert(e.target.value);
+};
+
+btn
